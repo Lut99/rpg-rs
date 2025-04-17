5115c477dea112211f07796511a0ed3295d121bc
Lut99 <mullertim99@gmail.com>
2025-04-17 22:23:10 +0200

    (Finally) Added some basic boilerplate

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG for `rpg-rs`
+This file keeps track of notable changes to the `rpg-rs` codebase.
+
+This project uses [semantic versioning](https://semver.org). As such, breaking changes are indicated with **(BREAKING)**.
+
+
+## v0.1.0 - TODO
+Initial release!
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+//  MAIN.rs
+//    by Lut99
+//
+//  Created:
+//    17 Apr 2025, 22:21:47
+//  Last edited:
+//    17 Apr 2025, 22:22:02
+//  Auto updated?
+//    Yes
+//
+//  Description:
+//!   The entrypoint to the game executable.
+//
+
+
+/***** ENTRYPOINT *****/
+fn main() {}
